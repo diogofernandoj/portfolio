be2446ea593b027551e765e727be099d402ead79
Diogo Jorge <diogofernandoj@gmail.com>
2024-08-05 11:29:45 -0300

    feat: add BackgroundHome component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import HomeSection from "./home/HomeSection";
 import Navbar from "./components/Navbar";
 import BenefitsSection from "./benefits/BenefitsSection";
+import BackgroundHome from "./components/BackgroundHome";
 
 const App = () => {
   return (
     <div className="text-white">
       <Navbar />
       <HomeSection />
+      <BackgroundHome />
       <BenefitsSection />
     </div>
   );
