cd5cb049cfbe4c5876f45745857e41be2c586f7a
Diogo Jorge <diogofernandoj@gmail.com>
2024-08-07 11:54:04 -0300

    feat: add texts to HomeSection

--- src/home/HomeSection.tsx
+++ src/home/HomeSection.tsx
@@ -3,23 +3,21 @@ import Links from "./components/Links";
 
 const HomeSection = () => {
   return (
-    <div className="w-[90%] md:w-[60%] lg:w-[50%] h-screen mx-auto pt-20 flex flex-col gap-4 justify-center items-center text-center">
-      <h2 className="font-bold text-xl md:text-3xl lg:text-4xl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus at hic
-        dolorem?
+    <div className="w-[90%] max-w-3xl md:w-[60%] lg:w-[50%] h-[calc(100vh-7rem)] lg:h-[calc(100vh-10rem)] mx-auto md:pt-40 pt-24 flex flex-col gap-4 justify-center items-center text-center">
+      <img src="src/assets/images/profile-pic.svg" className="h-24 w-24" />
+      <h2 className="font-bold text-2xl md:text-3xl lg:text-4xl">
+        Eleve seu negócio ao próximo nível{" "}
+        <span className="text-brand-primary">através da tecnologia</span>!
       </h2>
-      <p className="text-gray-400 text-sm md:text-base lg:text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, qui est
-        odio id quisquam eveniet.
+      <p className="text-gray-400 font-semibold lg:text-lg">
+        Eu sou o Diogo Jorge, desenvolvedor Full-Stack e aumento a lucratividade
+        da sua empresa com soluções inovadoras.
       </p>
       <Links />
       <a
         href="#benefits"
-        className="bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-2 rounded-full font-semibold shadow-lg shadow-cyan-500/50 transition hover:shadow-blue-500/50"
+        className="absolute bottom-0 animate-bounce transition hover:text-brand-primary"
       >
-        Saiba mais
-      </a>
-      <a href="#benefits" className="absolute bottom-0 animate-bounce">
         <MdKeyboardDoubleArrowDown size={36} />
       </a>
     </div>
