7fdaaff77c16c47335e555f34e6a60eb2f4f0b54
Diogo Jorge <diogofernandoj@gmail.com>
2024-08-07 09:19:51 -0300

    feat: add initial SkillsSection

--- src/skills/SkillsSection.tsx
+++ src/skills/SkillsSection.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from "react";
 import {
   BiLogoJavascript,
   BiLogoPostgresql,
@@ -13,67 +12,58 @@ const SkillsSection = () => {
   const logos = [
     {
       id: 1,
-      icon: <BiLogoJavascript />,
+      icon: <BiLogoJavascript size={50} />,
     },
     {
       id: 2,
-      icon: <FaReact />,
+      icon: <FaReact size={50} />,
     },
     {
       id: 3,
-      icon: <FaNode />,
+      icon: <FaNode size={50} />,
     },
     {
       id: 4,
-      icon: <BiLogoTypescript />,
+      icon: <BiLogoTypescript size={50} />,
     },
     {
       id: 5,
-      icon: <BiLogoPostgresql />,
+      icon: <BiLogoPostgresql size={50} />,
     },
     {
       id: 6,
-      icon: <SiPrisma />,
+      icon: <SiPrisma size={50} />,
     },
     {
       id: 7,
-      icon: <RiNextjsFill />,
+      icon: <RiNextjsFill size={50} />,
     },
     {
       id: 8,
-      icon: <BiLogoTailwindCss />,
+      icon: <BiLogoTailwindCss size={50} />,
     },
     {
       id: 9,
-      icon: <SiJest />,
+      icon: <SiJest size={50} />,
     },
     {
       id: 10,
-      icon: <FaDocker />,
+      icon: <FaDocker size={50} />,
     },
   ];
-
-  const carouselRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (carouselRef.current) {
-      const clonedItems = carouselRef.current.innerHTML;
-      carouselRef.current.insertAdjacentHTML("beforeend", clonedItems);
-    }
-  }, []);
 
   return (
     <div className="overflow-hidden relative">
       <h2 className="text-center font-bold text-3xl mt-12 mb-8">
         Tecnologias<span className="text-brand-primary">.</span>
       </h2>
-      <div ref={carouselRef} className="flex gap-6 w-auto animate-slide">
-        {logos.map((logo) => (
+      <div className="flex gap-6 w-auto animate-slide">
+        {logos.concat(logos).map((logo) => (
           <div
             key={logo.id}
             className="flex-none w-20 h-20 flex items-center justify-center"
           >
-            <span className="text-5xl opacity-10">{logo.icon}</span>
+            <span className="opacity-10">{logo.icon}</span>
           </div>
         ))}
       </div>
