bdfeb4439000828457011222a4e8e3579fec5d27
Diogo Jorge <diogofernandoj@gmail.com>
2024-08-07 23:16:52 -0300

    feat: add initial AboutSection

--- src/about/AboutSection.tsx
+++ src/about/AboutSection.tsx
@@ -2,27 +2,34 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const AboutSection = () => {
   return (
-    <div className="flex flex-col gap-12 p-5 items-center">
-      <h2 className="text-4xl font-bold">
-        Quem sou eu <span className="text-brand-primary">?</span>
-      </h2>
+    <div className="flex justify-center items-center my-12 p-5">
       <div className="lg:flex-row lg:w-[70%] max-w-5xl flex flex-col items-center gap-12 w-[80%]">
         <img
           src="src/assets/images/guy-with-certificate.svg"
           className="w-60"
         />
         <div className="w-full flex flex-col gap-8">
-          <p className="text-xl font-semibold text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            quaerat minima, cumque nam exercitationem, officia dolorem cum
-            debitis quidem distinctio a. Ea odit eligendi harum, sapiente
-            quaerat aperiam dolorem ratione.
+          <h2 className="md:text-4xl text-3xl font-bold">
+            Diogo Fernando S. Jorge
+          </h2>
+          <p className="md:text-xl text-lg font-semibold text-brand-text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
+            autem accusamus doloremque minima, esse, soluta aliquid iste
+            similique adipisci voluptatem facere optio veniam unde suscipit
+            illum. Vel dolorum temporibus suscipit!
+          </p>
+          <p className="md:text-xl text-lg font-semibold text-brand-text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
+            autem accusamus doloremque minima, esse, soluta aliquid iste
+            similique adipisci voluptatem facere optio veniam unde suscipit
+            illum. Vel dolorum temporibus suscipit!
           </p>
           <a
             href="https://linkwhats.app/445301"
-            className="bg-brand-primary text-white text-lg font-semibold flex items-center gap-1 px-6 w-max rounded-full py-2 transition shadow-lg hover:bg-white hover:text-brand-primary"
+            target="blank"
+            className="w-max px-4 py-2 rounded-full bg-brand-border flex items-center gap-2 font-bold hover:opacity-75"
           >
-            Contate-me <FaWhatsapp size={24} />
+            Contate-me <FaWhatsapp size={20} />
           </a>
         </div>
       </div>
