8c63ddf8438e3aa7229210611f1e8f38255aca26
Diogo Jorge <diogofernandoj@gmail.com>
2024-08-07 23:17:50 -0300

    style: ensure BackgroundHome component has max width

--- src/components/BackgroundHome.tsx
+++ src/components/BackgroundHome.tsx
@@ -2,7 +2,8 @@ const BackgroundHome = () => {
   return (
     <div className="lg:h-40 relative h-28 z-[-1]">
       <div
-        className="lg:w-[calc(100%-8rem)] md:w-[calc(100%-4rem)] absolute top-0 left-0 right-0 mx-auto w-[calc(100%-2rem)] h-full opacity-20"
+        className="
+       max-w-7xl lg:w-[calc(100%-8rem)] md:w-[calc(100%-4rem)] absolute top-0 left-0 right-0 mx-auto w-[calc(100%-2rem)] h-full opacity-20"
         style={{
           backgroundImage: "url('src/assets/images/background.png')",
           backgroundPosition: "top center",
