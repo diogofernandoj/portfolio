bf27a2a43cf69b5f62f251aae37b84f75c3c0c23
Diogo Jorge <diogofernandoj@gmail.com>
2024-08-07 23:18:25 -0300

    style: change BenefitsSection texts to text color

--- src/benefits/BenefitsSection.tsx
+++ src/benefits/BenefitsSection.tsx
@@ -3,20 +3,20 @@ import { RiWhatsappFill } from "react-icons/ri";
 
 const BenefitsSection = () => {
   return (
-    <div className="flex flex-col gap-12 p-8 lg:p-20 lg:flex-row min-h-screen">
-      <div className="flex flex-col gap-4 lg:w-2/5 lg:sticky lg:top-24 h-max">
-        <h2 className="text-3xl font-bold mb-4">
+    <div className="flex flex-col gap-12 mt-12 p-8 lg:p-20 lg:flex-row min-h-screen">
+      <div className="flex flex-col gap-4 lg:w-2/5 lg:sticky lg:top-24 h-max mb-10">
+        <h2 className="text-3xl font-bold mb-2">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
           veniam?
         </h2>
-        <p>
+        <p className="text-brand-text">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa harum
           eaque odit delectus repellendus est alias aspernatur vel illum iste.
         </p>
         <a
           href="https://linkwhats.app/445301"
           target="blank"
-          className="text-brand-primary font-semibold flex items-center gap-2 transition hover:underline pt-8"
+          className="text-brand-primary font-semibold flex items-center gap-2 transition hover:underline mt-4"
         >
           <span className="bg-brand-primary text-white rounded-full p-2">
             <RiWhatsappFill size={18} />
@@ -26,14 +26,14 @@ const BenefitsSection = () => {
       </div>
       <div className="flex flex-col gap-20 lg:w-3/5 overflow-y-auto">
         <div className="flex gap-4">
-          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2">
+          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2 mt-2">
             <FaCheck size={18} />
           </span>
           <div className="flex flex-col">
             <h3 className="text-2xl font-semibold mb-2">
               Lorem ipsum dolor sit amet consectetur.
             </h3>
-            <p className="opacity-50">
+            <p className="text-brand-text">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
               animi suscipit cum, voluptatibus optio id itaque officia
               perspiciatis consequuntur hic earum consequatur?
@@ -41,14 +41,14 @@ const BenefitsSection = () => {
           </div>
         </div>
         <div className="flex gap-4">
-          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2">
+          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2 mt-2">
             <FaCheck size={18} />
           </span>
           <div className="flex flex-col">
             <h3 className="text-2xl font-semibold mb-2">
               Lorem ipsum dolor sit amet consectetur.
             </h3>
-            <p className="opacity-50">
+            <p className="text-brand-text">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
               animi suscipit cum, voluptatibus optio id itaque officia
               perspiciatis consequuntur hic earum consequatur?
@@ -56,14 +56,14 @@ const BenefitsSection = () => {
           </div>
         </div>
         <div className="flex gap-4">
-          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2">
+          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2 mt-2">
             <FaCheck size={18} />
           </span>
           <div className="flex flex-col">
             <h3 className="text-2xl font-semibold mb-2">
               Lorem ipsum dolor sit amet consectetur.
             </h3>
-            <p className="opacity-50">
+            <p className="text-brand-text">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
               animi suscipit cum, voluptatibus optio id itaque officia
               perspiciatis consequuntur hic earum consequatur?
@@ -71,14 +71,14 @@ const BenefitsSection = () => {
           </div>
         </div>
         <div className="flex gap-4">
-          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2">
+          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2 mt-2">
             <FaCheck size={18} />
           </span>
           <div className="flex flex-col">
             <h3 className="text-2xl font-semibold mb-2">
               Lorem ipsum dolor sit amet consectetur.
             </h3>
-            <p className="opacity-50">
+            <p className="text-brand-text">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
               animi suscipit cum, voluptatibus optio id itaque officia
               perspiciatis consequuntur hic earum consequatur?
@@ -86,14 +86,14 @@ const BenefitsSection = () => {
           </div>
         </div>
         <div className="flex gap-4">
-          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2">
+          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2 mt-2">
             <FaCheck size={18} />
           </span>
           <div className="flex flex-col">
             <h3 className="text-2xl font-semibold mb-2">
               Lorem ipsum dolor sit amet consectetur.
             </h3>
-            <p className="opacity-50">
+            <p className="text-brand-text">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
               animi suscipit cum, voluptatibus optio id itaque officia
               perspiciatis consequuntur hic earum consequatur?
@@ -101,14 +101,14 @@ const BenefitsSection = () => {
           </div>
         </div>
         <div className="flex gap-4">
-          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2">
+          <span className="bg-brand-primary text-white rounded-full h-max w-max p-2 mt-2">
             <FaCheck size={18} />
           </span>
           <div className="flex flex-col">
             <h3 className="text-2xl font-semibold mb-2">
               Lorem ipsum dolor sit amet consectetur.
             </h3>
-            <p className="opacity-50">
+            <p className="text-brand-text">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
               animi suscipit cum, voluptatibus optio id itaque officia
               perspiciatis consequuntur hic earum consequatur?
