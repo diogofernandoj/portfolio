56b908ce5df90e7efdf31cad2e0661f5b88f8ec0
Diogo Jorge <diogofernandoj@gmail.com>
2024-08-09 18:57:34 -0300

    feat: add benefits const

---
+++ src/constants/benefits.ts
@@ -0,0 +1,38 @@
+export const BENEFITS = [
+  {
+    id: 1,
+    header: "Lorem ipsum dolor sit amet consectetur.",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione animi suscipit cum, voluptatibus optio id itaque officia perspiciatis consequuntur hic earum consequatur?",
+  },
+  {
+    id: 2,
+    header: "Lorem ipsum dolor sit amet consectetur.",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione animi suscipit cum, voluptatibus optio id itaque officia perspiciatis consequuntur hic earum consequatur?",
+  },
+  {
+    id: 3,
+    header: "Lorem ipsum dolor sit amet consectetur.",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione animi suscipit cum, voluptatibus optio id itaque officia perspiciatis consequuntur hic earum consequatur?",
+  },
+  {
+    id: 4,
+    header: "Lorem ipsum dolor sit amet consectetur.",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione animi suscipit cum, voluptatibus optio id itaque officia perspiciatis consequuntur hic earum consequatur?",
+  },
+  {
+    id: 5,
+    header: "Lorem ipsum dolor sit amet consectetur.",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione animi suscipit cum, voluptatibus optio id itaque officia perspiciatis consequuntur hic earum consequatur?",
+  },
+  {
+    id: 6,
+    header: "Lorem ipsum dolor sit amet consectetur.",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione animi suscipit cum, voluptatibus optio id itaque officia perspiciatis consequuntur hic earum consequatur?",
+  },
+];
